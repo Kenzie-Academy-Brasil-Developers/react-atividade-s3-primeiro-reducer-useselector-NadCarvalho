df9a265e49c75ef1f93a219507b25af10f4c1a1d
NadCarvalho <carvalho.nadiacaroline@gmail.com>
2021-09-27 18:33:22 -0300

    feat: FruitsPage com reducer+UseSelector

---
+++ src/pages/Fruitspage.jsx
@@ -0,0 +1,17 @@
+//importando useSelector do react-redux
+import { useSelector } from "react-redux";
+
+const FruitsPage = () => {
+
+  const fruits = useSelector((state) => state.fruits);
+
+  return (
+    <div>
+      {fruits.map((fruit) => (
+        <p key={fruit}> {fruit} </p>
+      ))}
+    </div>
+  );
+}
+
+export default FruitsPage;
